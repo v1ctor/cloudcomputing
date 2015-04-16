c937afd13520926f7186efb2b6cd48fc9597a4b6
Victor Buldakov <victor@buldakov.org>
2015-04-16 23:05:59 +0300

    merge mp1

--- mp2_assignment/MP1Node.h
+++ mp2_assignment/MP1Node.h
@@ -28,10 +28,11 @@
 /**
  * Message Types
  */
-enum MsgTypes{
-    JOINREQ,
-    JOINREP,
-    DUMMYLASTMSGTYPE
+enum MsgTypes {
+	JOINREQ,
+	JOINREP,
+	HEARTBEAT,
+	DUMMYLASTMSGTYPE
 };
 
 /**
@@ -55,6 +56,7 @@ private:
 	Params *par;
 	Member *memberNode;
 	char NULLADDR[6];
+	vector<int> deleted;
 
 public:
 	MP1Node(Member *, Params *, EmulNet *, Log *, Address *);
@@ -75,7 +77,13 @@ public:
 	Address getJoinAddress();
 	void initMemberListTable(Member *memberNode);
 	void printAddress(Address *addr);
+	void sprintAddress(char const * format, char* buffer, Address *addr);
+	bool isDeleted(int id);
+	void removeFromDeleted(int id);
+	void logMemberList(vector<MemberListEntry>* list, Address* addr);
 	virtual ~MP1Node();
+
+	void mergeTable(vector<MemberListEntry> *pVector);
 };
 
 #endif /* _MP1NODE_H_ */
